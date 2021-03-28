19884029f6bb43460a1a025485c3d77cef55b866
imas8888 <35904771+imas8888@users.noreply.github.com>
2021-03-28 13:58:58 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+import sys
+from joblib import load
+
+model = load('LLC.pkl')
+
+for line in sys.stdin:
+    inputD = np.array([float(i) for i in line.split(",")]).reshape(1,-1)
+    print(int(model.predict(inputD)[0]))
